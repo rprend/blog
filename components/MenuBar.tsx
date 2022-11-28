0843d1e20e07b5914de804bafc92709c9610db74
Ryan Prendergast <rprendergast1121@gmail.com>
2022-11-27 18:32:02 -0600

    Add basic formatting to editor

---
+++ components/MenuBar.tsx
@@ -0,0 +1,25 @@
+import { Editor } from "@tiptap/react";
+import React from "react";
+
+export interface MenuBarProps {
+  editor: Editor | null;
+}
+
+export const MenuBar = (props: MenuBarProps) => {
+  return (
+    <div>
+      <button onClick={() => props.editor?.chain().focus().toggleBold().run()}>Bold</button>
+      <button onClick={() => props.editor?.chain().focus().toggleItalic().run()}>Italic</button>
+      <button onClick={() => props.editor?.chain().focus().toggleCode().run()}>Code</button>
+      <button onClick={() => props.editor?.chain().focus().toggleStrike().run()}>Strike</button>
+      <button onClick={() => props.editor?.chain().focus().toggleBulletList().run()}>Bullet List</button>
+      <button onClick={() => props.editor?.chain().focus().toggleOrderedList().run()}>Ordered List</button>
+      <button onClick={() => props.editor?.chain().focus().toggleHeading({ level: 1 }).run()}>Heading 1</button>
+      <button onClick={() => props.editor?.chain().focus().toggleHeading({ level: 2 }).run()}>Heading 2</button>
+      <button onClick={() => props.editor?.chain().focus().toggleHeading({ level: 3 }).run()}>Heading 3</button>
+      <button onClick={() => props.editor?.chain().focus().toggleBlockquote().run()}>Blockquote</button>
+      <button onClick={() => props.editor?.chain().focus().toggleCodeBlock().run()}>Code Block</button>
+      <button onClick={() => props.editor?.chain().focus().setHorizontalRule().run()}>Horizontal Rule</button>
+    </div>
+  )
+}
--- components/Tiptap.tsx
+++ components/Tiptap.tsx
@@ -1,17 +1,28 @@
 import { useEditor, EditorContent } from "@tiptap/react";
+import CharacterCount from '@tiptap/extension-character-count'
 import StarterKit from "@tiptap/starter-kit";
 import React from "react";
+import { MenuBar } from "./MenuBar";
 
 const Tiptap = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      CharacterCount,
     ],
     content: "<p>Hello World!</p>",
   })
 
   return (
-    <EditorContent editor={editor} />
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+      <div className="character-count">
+        {editor?.storage.characterCount.characters()} characters
+        <br />
+        {editor?.storage.characterCount.words()} words
+      </div>
+    </div>
   )
 }
 
