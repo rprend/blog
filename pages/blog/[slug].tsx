6858bff5d5585e6a9b1d596e6d6262ac8dfcea8d
Ryan Prendergast <rprendergast1121@gmail.com>
2022-11-27 22:56:02 -0600

    Prerender all blog posts

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -5,7 +5,6 @@ import Tiptap from '../../components/Tiptap'
 
 export default function Post(params) {
   const post = JSON.parse(params.post)
-  console.log(post.content)
 
   return (
     <main>
@@ -20,27 +19,19 @@ export default function Post(params) {
 }
 
 export async function getStaticPaths() {
-  // const paths = readPosts()
-  // console.log("GETTING STATIC PATHS")
-  // debugger
-  // console.log(paths)
-  // const second = paths.map((post) => {
-  //   return { params: post.slug }
-  // })
-  // console.log("PATHS: " + paths)
-  const paths = readPosts()
+  const paths = readPosts().map((post) => {
+    return { params: { slug: post.slug } }
+  })
 
-  const last = [{ params: { slug: 'abcde' } }, { params: { slug: 'test-post-2' } }]
   return {
-    paths: last,
+    paths,
     fallback: false
   }
 }
 
 export function getStaticProps({ params }) {
-  console.log(params)
   const post = fs.readFileSync(path.join(process.cwd(), 'posts', `${params.slug}.json`), 'utf8')
-  console.log(post)
+
   return {
     props: { post }
   }
