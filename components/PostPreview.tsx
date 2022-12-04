c8ac23887f1521705873eee4125ee0766a0c5480
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-04 13:16:39 -0500

    Remove date from post preview

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -9,7 +9,6 @@ export const PostPreview = (props: PostPreviewProps) => {
   return (
     <div className="post-preview">
       <h3 className="post-title">{props.title}</h3>
-      {props.date && <p>Posted on {props.date}</p>}
       <p>{props.excerpt}...</p>
     </div>
   )
