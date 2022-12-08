9d41723b62ae2c1d4c958e3aae160e893d646706
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-07 22:59:42 -0500

    Stupid linter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import fs from 'fs';
-import path from 'path';
 
 export const authOptions = {
   secret: process.env.SECRET,
   providers: [
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: { label: "Username", type: "text", placeholder: "jonfken" },
-    //     password: { label: "Password", type: "password" }
-    //   },
-    //   async authorize(credentials, req) {
-    //     if (credentials.username === process.env.USERNAME && credentials.password === process.env.PASSWORD) {
-    //       return {
-    //         id: 1,
-    //         name: "test account",
-    //         email: "nstet@hotmail.com"
-    //       }
-    //     }
-    //     return null
-    //   }
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
