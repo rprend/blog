9a64072bfddd8a452bfc937101d6b10a20e28a91
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-14 20:09:44 -0500

    About page, mobile margin

---
+++ pages/about.tsx
@@ -0,0 +1,10 @@
+import Topbar from "../components/Topbar";
+
+export default function About() {
+  return (
+    <main>
+      <Topbar></Topbar>
+      indeed we&aposve got a blog. it has a long and storied history.
+    </main>
+  )
+}
