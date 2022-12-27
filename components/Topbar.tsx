548003732fa6d68ddd7ff7c245b289385d3e37b9
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-26 22:08:14 -0600

    Add admin and create page if logged in

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,12 +1,21 @@
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function Topbar() {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col mt-16 mb-4">
       <h1 className="text-4xl self-center">NIRI Blog</h1>
       <div className="flex flex-row gap-x-4 justify-center">
         <p><Link href='/'>Home</Link></p>
         <p><Link href='/about'>About</Link></p>
+        {session &&
+          <>
+          <p><Link href='/create'>Create</Link></p>
+          <p><Link href='/admin'>Admin</Link></p>
+          </>
+        }
       </div>
     </div>
   )
