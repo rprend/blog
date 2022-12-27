eacd288d15f9555ffa74bed8bc566e5460475149
Ryan Prendergast <rprendergast1121@gmail.com>
2022-12-26 22:20:51 -0600

    Bold up present route

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,19 +1,27 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import classNames from "classnames";
+import { useRouter } from "next/router";
+
+export type page = "home" | "about" | "create" | "admin";
+
+export interface TopbarProps {
+  page: page;
+}
 
 export default function Topbar() {
   const { data: session } = useSession();
+  const router = useRouter()
 
   return (
     <div className="flex flex-col mt-16 mb-4">
-      <h1 className="text-4xl self-center">NIRI Blog</h1>
-      <div className="flex flex-row gap-x-4 justify-center">
-        <p><Link href='/'>Home</Link></p>
-        <p><Link href='/about'>About</Link></p>
+      <h1 className="text-4xl">NIRI Blog</h1>
+      <div className={"flex flex-row gap-x-4 mt-4 " + classNames({'italic': session})}>
+        <p><Link href='/' className={classNames({'font-bold': router.pathname === '/'})}>Home</Link></p>
+        <p><Link href='/about' className={classNames({'font-bold': router.pathname === '/about'})}>About</Link></p>
         {session &&
           <>
-          <p><Link href='/create'>Create</Link></p>
-          <p><Link href='/admin'>Admin</Link></p>
+          <p><Link href='/admin' className={classNames({'font-bold': router.pathname === '/admin'})}>Admin</Link></p>
           </>
         }
       </div>
